19c853d8d3bac09c55171b67e7363eafb0ae3b3d
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-06-06 20:57:10 +0300

    Print initial text to vga buffer

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,18 @@ fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
+const INITIAL_TEXT: &[u8] = b"Hello, World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (index, value) in INITIAL_TEXT.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(index as isize * 2) = *value;
+            *vga_buffer.offset(index as isize * 2 + 1) = 0xA;
+        }
+    }
+
     loop {}
 }
