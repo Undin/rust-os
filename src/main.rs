00377b09389137a3951ef833ace24f9218ccfa24
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-12-22 20:22:03 +0300

    Redirect test output from QEMU to host stdout via serial port

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,27 @@
 #![reexport_test_harness_main = "test_entry_point"]
 
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}", info);
     #[cfg(test)]
     exit_qemu(QemuExitCode::Fail);
     loop {}
 }
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -30,7 +41,7 @@ pub extern "C" fn _start() -> ! {
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -55,14 +66,14 @@ fn exit_qemu(exit_code: QemuExitCode) {
 
 #[test_case]
 fn trivial_test_ok() {
-    print!("trivial assertion...");
+    serial_print!("trivial assertion ok...");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
 
 #[test_case]
 fn trivial_test_fail() {
-    print!("trivial assertion...");
+    serial_print!("trivial assertion fail...");
     assert_eq!(1, 2);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
---
+++ src/serial.rs
@@ -0,0 +1,32 @@
+use core::fmt;
+use core::fmt::Write;
+
+use lazy_static::lazy_static;
+use spin::Mutex;
+use uart_16550::SerialPort;
+
+lazy_static! {
+    pub static ref SERIAL1: Mutex<SerialPort> = {
+        let mut port = unsafe { SerialPort::new(0x3F8) };
+        port.init();
+        Mutex::new(port)
+    };
+}
+
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => { $crate::serial::_print(format_args!($($arg)*)) };
+}
+
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($($arg:tt)*) => ({
+        $crate::serial_print!("{}\n", format_args!($($arg)*));
+    })
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    SERIAL1.lock().write_fmt(args).unwrap();
+}
