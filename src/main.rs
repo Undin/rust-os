f1a0fac98dbcd941e066c39a197019adf7da13cc
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-12-22 16:29:35 +0300

    Initial implementation of test launch

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#![reexport_test_harness_main = "test_entry_point"]
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -14,6 +19,31 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, World!");
-    print!("Some numbers: {} {}", 1, 2.345);
+
+    #[cfg(test)]
+    test_entry_point();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_test_ok() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[test_case]
+fn trivial_test_fail() {
+    print!("trivial assertion...");
+    assert_eq!(1, 2);
+    println!("[ok]");
 }
