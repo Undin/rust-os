7b898e8205980977fe886d11ab938095814d851e
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-12-25 14:44:57 +0300

    Extract all code related to tests into library

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#![reexport_test_harness_main = "test_entry_point"]
+
+use core::panic::PanicInfo;
+
+pub mod vga_buffer;
+pub mod serial;
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}", info);
+    exit_qemu(QemuExitCode::Fail);
+    loop {}
+}
+
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_entry_point();
+    loop {}
+}
+
+#[doc(hidden)]
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T: Fn()> Testable for T {
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]")
+    }
+}
+
+#[doc(hidden)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success)
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Fail = 0x11
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::new(0xf4);
+    unsafe {
+        port.write(exit_code as u32);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,13 @@
 #![no_main]
 
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 
 #![reexport_test_harness_main = "test_entry_point"]
 
-use core::any;
 use core::panic::PanicInfo;
 
-mod vga_buffer;
-mod serial;
+use rust_os::println;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -22,13 +20,8 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}", info);
-    #[cfg(test)]
-    exit_qemu(QemuExitCode::Fail);
-    loop {}
+    rust_os::test_panic_handler(info);
 }
-
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -40,31 +33,6 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success)
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-#[repr(u32)]
-enum QemuExitCode {
-    Success = 0x10,
-    Fail = 0x11
-}
-
-fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    let mut port = Port::new(0xf4);
-    unsafe {
-        port.write(exit_code as u32);
-    }
-}
-
 #[test_case]
 fn trivial_test_ok() {
     assert_eq!(1, 1);
@@ -73,16 +41,4 @@ fn trivial_test_ok() {
 #[test_case]
 fn trivial_test_fail() {
     assert_eq!(1, 2);
-}
-
-trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T: Fn()> Testable for T {
-    fn run(&self) -> () {
-        serial_print!("{}...\t", any::type_name::<T>());
-        self();
-        serial_println!("[ok]")
-    }
 }
