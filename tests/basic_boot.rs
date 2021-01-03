9be56d1fcf583480129d1b74459ff2272fb8f31d
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-01-03 17:59:15 +0300

    Basic integration test

---
+++ tests/basic_boot.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+
+#![reexport_test_harness_main = "test_entry_point"]
+
+use core::panic::PanicInfo;
+
+use rust_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_entry_point();
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+
+#[test_case]
+fn test_println() {
+    println!("test");
+}
