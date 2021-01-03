b5f93f08c15fae06b11e3f13217abe9d12685e4c
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-01-03 19:46:57 +0300

    Use "harness = false" for should_panic integration test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,18 +1,15 @@
 #![no_std]
 #![no_main]
 
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-
-#![reexport_test_harness_main = "test_entry_point"]
-
 use core::panic::PanicInfo;
 
 use rust_os::{exit_qemu, QemuExitCode, serial_print, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_entry_point();
+    should_panic();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Fail);
     loop {}
 }
 
@@ -23,17 +20,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
 fn should_panic() {
     serial_print!("should_panic::should_panic...\t");
     assert_eq!(1, 2);
-}
-
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Fail);
-    }
-    exit_qemu(QemuExitCode::Success);
 }
