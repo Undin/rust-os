4c01a71ea34b2fbe262f4986a2013236e68b23c5
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-09-26 00:22:08 +0300

    Support keyboard interrupts, decode and print pressed keys

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,8 +3,8 @@ use pic8259::ChainedPics;
 use spin::Mutex;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-use crate::gdt;
 use crate::{print, println};
+use crate::gdt;
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -17,6 +17,7 @@ lazy_static! {
         }
 
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyborad_interrupt_handler);
         idt
     };
 }
@@ -36,6 +37,41 @@ extern "x86-interrupt" fn timer_interrupt_handler(_frame: InterruptStackFrame) {
     }
 }
 
+// TODO: move these imports and lazy_static macro call to `keyborad_interrupt_handler`
+use pc_keyboard::{
+    DecodedKey,
+    HandleControl,
+    Keyboard,
+    layouts::Us104Key,
+    ScancodeSet1,
+};
+
+lazy_static! {
+    static ref KEYBOARD: Mutex<Keyboard<Us104Key, ScancodeSet1>> =
+        Mutex::new(Keyboard::new(Us104Key, ScancodeSet1, HandleControl::Ignore));
+}
+
+extern "x86-interrupt" fn keyborad_interrupt_handler(_frame: InterruptStackFrame) {
+    use x86_64::instructions::port::Port;
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+    let scancode: u8 = unsafe { port.read() };
+
+    if let Ok(Some(event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(event) {
+            match key {
+                DecodedKey::RawKey(k) => print!("{:?}", k),
+                DecodedKey::Unicode(c) => print!("{}", c)
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
+}
+
 pub fn init_idt() {
     IDT.load()
 }
@@ -47,7 +83,8 @@ pub static PICS: Mutex<ChainedPics> = Mutex::new(unsafe { ChainedPics::new(PIC1_
 
 #[repr(u8)]
 pub enum InterruptIndex {
-    Timer = PIC1_OFFSET
+    Timer = PIC1_OFFSET,
+    Keyboard
 }
 
 //noinspection RsSelfConvention
